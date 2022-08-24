6c64afe6531fc34ee1721b059157b51e78bbc293
Manda Putra <mandaputra8@gmail.com>
2022-08-24 20:15:12 +0700

    setup jest testing

---
+++ test/pages/login.spec.js
@@ -0,0 +1,9 @@
+import Home from '../pages/login'
+import '@testing-library/jest-dom'
+import { fireEvent, render, screen } from '@testing-library/react'
+
+describe('test module login', () => {
+  test('halaman keluar dengan benar', () => {
+    render()
+  })
+})
